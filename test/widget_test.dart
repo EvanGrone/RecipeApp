5fd1ce74ac059b97e8bd59240efc7528f989af1a
EvanGrone <106884776+EvanGrone@users.noreply.github.com>
2023-12-07 15:00:34 -0500

    Updating Test files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,27 +4,86 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_github_workflow/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Find Matching Recipes Dialog', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Set up some selected ingredients
+    final ingredientsApp =
+        tester.firstWidget(find.byType(IngredientsApp)) as IngredientsApp;
+    final ingredientsAppState = ingredientsApp.createState();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    ingredientsAppState.selectedIngredients = [
+      'Flour',
+      'Eggs',
+      'Sugar',
+    ];
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Trigger the _findMatchingRecipes method
+    await ingredientsAppState._findMatchingRecipes();
+
+    // Wait for the dialog to appear
+    await tester.pumpAndSettle();
+
+    // Verify that the dialog is shown
+    expect(find.text('Matching Recipes'), findsOneWidget);
+    expect(find.text('Blueberry Muffins'), findsOneWidget);
+  });
+
+  testWidgets('No Matching Recipes Dialog', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Set up some selected ingredients
+    const ingredientsAppState = IngredientsApp();
+
+    ingredientsAppState.selectedIngredients = [
+      'Chocolate',
+      'Water',
+    ];
+
+    // Trigger the _findMatchingRecipes method
+    await ingredientsAppState._findMatchingRecipes();
+
+    // Wait for the dialog to appear
+    await tester.pumpAndSettle();
+
+    // Verify that the dialog is shown
+    expect(find.text('No Matching Recipes'), findsOneWidget);
+    expect(
+      find.text('No recipes match the selected ingredients.'),
+      findsOneWidget,
+    );
+  });
+
+  testWidgets('Widget Test - Dropdown and Buttons',
+      (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that the initial state is correct
+    expect(find.text('Remove'), findsNothing);
+    expect(find.text('GO!'), findsOneWidget);
+
+    // Select an ingredient from the dropdown
+    await tester.tap(find.byType(DropdownButton));
+    await tester.pumpAndSettle();
+
+    // Verify that the ingredient is selected
+    expect(find.text('Remove'), findsOneWidget);
+    expect(find.text('GO!'), findsOneWidget);
+
+    // Remove the selected ingredient
+    await tester.tap(find.text('Remove'));
+    await tester.pumpAndSettle();
+
+    // Verify that the ingredient is removed
+    expect(find.text('Remove'), findsNothing);
+    expect(find.text('GO!'), findsOneWidget);
   });
 }
