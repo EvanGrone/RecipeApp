e210e5eb20b6b4ab9eef0c16c080b9d2c1456826
EvanGrone <106884776+EvanGrone@users.noreply.github.com>
2023-12-10 14:35:23 -0500

    more messages

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -27,4 +27,28 @@ void main() {
     // Verify if no ingredients are initially selected.
     expect(find.byType(ListTile), findsNothing);
   });
+
+  testWidgets('Ingredient selection and removal', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    // Tap on the dropdown and select an ingredient.
+    await tester.tap(find.byType(DropdownButton<String>));
+    await tester.pump();
+
+    // Verify if the selected ingredient is displayed.
+    expect(find.text('Remove'), findsOneWidget);
+
+    // Tap on the Remove button and check if the ingredient is removed.
+    await tester.tap(find.text('Remove'));
+    await tester.pump();
+
+    // Verify if the Remove button is there still.
+    expect(find.text('Remove'), findsOneWidget);
+
+    // Verify if the GO! button is still present.
+    expect(find.text('GO!'), findsOneWidget);
+
+    // Verify if no ingredients are selected after removal.
+    expect(find.byType(ListTile), findsOneWidget);
+  });
 }
